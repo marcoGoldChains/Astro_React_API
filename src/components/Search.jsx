f073a48b0fa061449b7f4a7df29ec25e7c000f91
marcoGoldChains <marcos.jimenezfernandez13@gmail.com>
2024-04-22 10:53:45 +0200

    Creando buscador con React integrado en Astro

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,39 @@
 import { useState } from "react";
 
-function Search() {
-    const [text, setText] = useState("Hola React");
+export default function Search(){
+    const [value, setValue] = useState('')
+    const buscar = (e) => setValue(e.target.value)
+    const url = "/results/"
 
-    return (
+    return(
         <div>
-            <h1>{text}</h1>
-            <button onClick={() => setText("Bienvenido a React")}>Click</button>
+            <center>
+                <h1 class="mt-5">Search Movie</h1>
+                <h2>{value}</h2>
+                <form action={url + value} method="POST">
+                    <input type="search" class="form-control p-3" 
+                    value={value} onChange={buscar} 
+                    placeholder="Search" />
+                </form>
+            </center>
         </div>
     )
 }
 
 
-export default Search;
+
+
+
+// function Search() {
+// const [text, setText] = useState("Hola React");
+
+// return (
+// <div>
+// <h1>{text}</h1>
+// <button onClick={() => setText("Bienvenido a React")}>Click</button>
+// </div>
+// )
+// }
+
+
+// export default Search;
